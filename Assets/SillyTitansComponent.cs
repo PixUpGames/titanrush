b53725970acb39d24ff6b5706d2d0cde38d52a13
SergeiPolev <55839857+SergeiPolev@users.noreply.github.com>
2022-06-22 02:44:32 +0400

    Silly titans improvement + water on first level

--- Assets/SillyTitansComponent.cs
+++ Assets/SillyTitansComponent.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class SillyTitansComponent : CuttableItem
 {
+    [SerializeField] private float speed;
     [SerializeField] private int reward;
     [SerializeField] private Rigidbody[] rigidbodies;
     [SerializeField] private Animator animator;
@@ -12,12 +13,25 @@ public class SillyTitansComponent : CuttableItem
     public override CuttableType GetCuttableType() => CuttableType.Silly_Titan;
     public override int ReceiveAward() => reward;
 
+    private bool activated = false;
+
     private void Awake()
     {
         onTriggerEnterComponent.OnEnter += OnPlayerEnter;
     }
+    private void Update()
+    {
+        if (!activated)
+        {
+            return;
+        }
+
+        transform.parent.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
     public override void Cut()
     {
+        activated = false;
+
         foreach (var rb in rigidbodies)
         {
             rb.isKinematic = false;
@@ -29,6 +43,8 @@ public class SillyTitansComponent : CuttableItem
     {
         if (other.CompareTag(playerTag))
         {
+            activated = true;
+
             animator.SetBool("Run", true);
         }
     }
