7715cda5aef44d9a6e200f58992478220bc08a69
Akfi23 <60118284+Akfi23@users.noreply.github.com>
2022-07-13 17:37:45 +0300

    Hammer boss fixes

--- Assets/Tech/Scripts/HammerEnemyComponent.cs
+++ Assets/Tech/Scripts/HammerEnemyComponent.cs
@@ -1,13 +1,17 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class HammerEnemyComponent : EnemyComponent
 {
     [SerializeField] private GameObject firstModel;
     [SerializeField] private GameObject secondModel;
+    [SerializeField] private ParticleSystem mutateParticle;
+    [SerializeField] private float scaleFactor;
     public override void Prepare()
     {
         secondModel.SetActive(true);
         firstModel.SetActive(false);
+        secondModel.transform.DOScale(Vector3.one*scaleFactor,0.35f).OnComplete(()=>mutateParticle.Play());
 
         base.Prepare();
     }
